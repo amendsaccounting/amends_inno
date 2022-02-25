90e7a04cfb645eb7066a9f35b16eea1e34dc5556
dineshpanchal93 <dineshpanchal432@gmail.com>
2022-02-25 11:24:42 +0000

    Update

--- amends_inno/amends_inno/report/foc_item_wise/foc_item_wise.js
+++ amends_inno/amends_inno/report/foc_item_wise/foc_item_wise.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Items-wise Sales Register"] = {
+frappe.query_reports["FOC Item wise"] = {
 	"filters": [
 		{
 			"fieldname":"from_date",
